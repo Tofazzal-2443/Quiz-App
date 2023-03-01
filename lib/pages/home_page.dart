63e51a036ced5bed34b65ab776524ab7838d8126
Tofazzal-2443 <tofazzal15-2443@diu.edu.bd>
2023-03-01 20:40:01 +0600

    Home Page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,10 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:listview/customwidget/quiz_item.dart';
+import 'package:listview/result_page.dart';
+import 'package:listview/temp_db.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,8 +14,95 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String genderGroupValue = 'Male';
+  Timer? timer;
+  int count = 20;
+  bool hasQuizStarted = false;
+
+  @override
+  void initState() {
+    quizList = generateQuizList;
+    super.initState();
+  }
+
+  void startTimer() {
+    timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (count > 0) {
+        setState(() {
+          count--;
+        });
+      } else {
+        stopTimer();
+        navigateToResult();
+      }
+    });
+  }
+
+  void stopTimer() {
+    if (timer != null) {
+      timer!.cancel();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Time Remains: $count sec'),
+          actions: [
+            if(hasQuizStarted) TextButton(
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.white,
+              ),
+              onPressed: () {
+                stopTimer();
+                navigateToResult();
+              },
+              child: const Text('Finish'),
+            ),
+            if(!hasQuizStarted) TextButton(
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.white,
+              ),
+              onPressed: () {
+                setState(() {
+                  hasQuizStarted = true;
+                });
+                startTimer();
+              },
+              child: const Text('Start'),
+            )
+          ],
+        ),
+        body: Stack(
+          children: [
+            ListView.builder(
+              padding: const EdgeInsets.all(12),
+              itemCount: quizList.length,
+              itemBuilder: (context, index) {
+                final quiz = quizList[index];
+                return QuizItem(
+                  serial: index + 1,
+                  quiz: quiz,
+                  onAnswer: (answer) {
+                    quizList[index].givenAnswer = answer;
+                  },
+                );
+              },
+            ),
+            if(!hasQuizStarted) Container(
+              color: Colors.white,
+              alignment: Alignment.center,
+              child: Text('Click Start button to start the Quiz'),
+
+            ),
+          ],
+        ));
+  }
+
+  void navigateToResult() {
+    Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const ResultPage()));
   }
 }
